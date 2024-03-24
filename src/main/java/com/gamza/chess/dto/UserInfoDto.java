7636e6883b34af4b2e7cb7310ebaf3689858dea6
leemj123 <love12mj@naver.com>
2024-03-24 15:48:27 +0900

    [hot fix] #1 userInfo 수정

--- src/main/java/com/gamza/chess/dto/UserInfoDto.java
+++ src/main/java/com/gamza/chess/dto/UserInfoDto.java
@@ -1,19 +1,17 @@
 package com.gamza.chess.dto;
 
-import com.gamza.chess.Enum.Tier;
-import com.gamza.chess.Enum.UserRole;
 import lombok.Getter;
 
 import java.util.Map;
 
 @Getter
 public class UserInfoDto {
-    private String userName;
-    private Object tier;
-    private int score;
+    private final String userName;
+    private final Object tier;
+    private final int score;
 
     public UserInfoDto (Map<String,Object> claims) {
-        this.userName = (String) claims.get("email");
+        this.userName = (String) claims.get("name");
         this.tier = claims.get("tier");
         this.score = (int) claims.get("score");
     }
